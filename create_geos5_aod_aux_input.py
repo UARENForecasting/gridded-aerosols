c551114ff9a691d062c822e79f1af22bb3dc6c3a
PatrickBunn <ptwbunn@email.arizona.edu>
2019-07-09 13:50:27 -0700

    add time out constraints, time monitoring and options for 12z and 00z runs

--- create_geos5_aod_aux_input.py
+++ create_geos5_aod_aux_input.py
@@ -4,6 +4,8 @@ import subprocess
 import glob
 import sys
 
+import requests
+import time
 import numpy as np
 import netCDF4 as nc
 import pandas as pd
@@ -14,6 +16,8 @@ import pandas as pd
 #                                                                               #
 # +84hrs of GEOS5 data is pulled, all data coming from 00z run                  # 
 #################################################################################
+time_limit = 2*3600 # [seconds]  2 hours 
+start_time = time.time()
 
 # find the day and forecast initialisation day
 # GEOS5 has ~8 hour latency
@@ -26,35 +30,53 @@ year  = time_now.strftime('%Y')
 month = time_now.strftime('%m')
 day   = time_now.strftime('%d')
 
-###################################
-# download 84hrs of 00z forecast  #
-###################################
-# if script is run when Z(now) is am then this is for the 06z WRF cycle  
-if time_now.hour < 12: 
+####################################################
+# download 84hrs of forecast based on current time #
+####################################################
+# this is for the 06z run  
+if 6 <= time_now.hour < 12: 
     forecast_times = [f'{hr:02}' for hr in range(6, 22, 3)] + [f'{hr:02}' for hr in range(0,22, 3)] *3 + ['00']
     forecast_days  = [time_now] * 6 + [time_now + pd.Timedelta('1d')] * 8 \
                                     + [time_now + pd.Timedelta('2d')] * 8 + [time_now + pd.Timedelta('3d')] * 8 \
                                     + [time_now + pd.Timedelta('4d')]
-# if script is run when Z(now) is pm then this is for the 18z WRF cycle 
-else:
-    forecast_times = ['18','21'] + [f'{hr:02}' for hr in range(0, 22, 3)] * 3 + [f'{hr:02}' for hr in range(0,13, 3)]
-    forecast_days = [time_now]* 2 + [time_now + pd.Timedelta('1d')] * 8 + [time_now + pd.Timedelta('2d')] * 8 \
-                                  + [time_now + pd.Timedelta('3d')] * 8 + [time_now + pd.Timedelta('4d')] * 5
-
+# this is for the 12z run  
+if 12 <= time_now.hour < 18:
+    forecast_times = [f'{hr:02}' for hr in range(12, 22, 3)] + [f'{hr:02}' for hr in range(0,22, 3)] *3 + ['00','06','09']
+    forecast_days  = [time_now] * 4 + [time_now + pd.Timedelta('1d')] * 8 \
+                                    + [time_now + pd.Timedelta('2d')] * 8 + [time_now + pd.Timedelta('3d')] * 8 \
+                                    + [time_now + pd.Timedelta('4d')] * 3
+# this is for the 18z run 
+if  18 <= time_now.hour < 24 : 
+    forecast_times = ['18','21']    + [f'{hr:02}' for hr in range(0, 22, 3)] * 3 + [f'{hr:02}' for hr in range(0,13, 3)]
+    forecast_days  = [time_now] * 2 + [time_now + pd.Timedelta('1d')] * 8 + [time_now + pd.Timedelta('2d')] * 8 \
+                                    + [time_now + pd.Timedelta('3d')] * 8 + [time_now + pd.Timedelta('4d')] * 5
+# this is for the 00z run 
+if  0 <= time_now.hour < 6 : 
+    forecast_times = [f'{hr:02}' for hr in range(0, 22, 3)] * 3  + [f'{hr:02}' for hr in range(0,19, 3)]
+    forecast_days =  [time_now + pd.Timedelta('1d')] * 8 + [time_now + pd.Timedelta('2d')] * 8 \
+                   + [time_now + pd.Timedelta('3d')] * 8 + [time_now + pd.Timedelta('4d')] * 7
+ 
+#################
+# download data #
+#################
 for forecast_day, forecast_time in zip(forecast_days, forecast_times):
-    file_name = 'GEOS.fp.fcst.inst1_2d_hwl_Nx.' + year + month + day + '_00+' + forecast_day.strftime('%Y%m%d') + '_' + forecast_time + '00.V01.nc4'
-    print(file_name)
-    url = 'https://portal.nccs.nasa.gov/datashare/gmao_ops/pub/fp/forecast/Y'+year+'/M'+month+'/D'+day+'/H00/'+file_name
-    try:
-        retcode = subprocess.call(" wget --user=gmao_ops --password=''  " + url, shell=True)
-        if retcode < 0:
-            print("Child was terminated by signal", -retcode, file=sys.stderr)
-        else:
-            print("Child returned", retcode, file=sys.stderr)
-    except OSError as e:
-        print("Execution failed:", e, file=sys.stderr)
-
-   
+    # stop download if passed time limit
+    if time.time() - start_time < time_limit:       
+        file_name = 'GEOS.fp.fcst.inst1_2d_hwl_Nx.' + year + month + day + '_00+' + forecast_day.strftime('%Y%m%d') + '_' + forecast_time + '00.V01.nc4'
+        print(file_name)
+        url = 'https://portal.nccs.nasa.gov/datashare/gmao_ops/pub/fp/forecast/Y'+year+'/M'+month+'/D'+day+'/H00/'+file_name
+        try:
+            retcode = subprocess.call(" wget --user=gmao_ops --password=''  " + url, shell=True)
+            if retcode < 0:
+                print("Child was terminated by signal", -retcode, file=sys.stderr)
+            else:
+                print("Child returned", retcode, file=sys.stderr)
+        except OSError as e:
+            print("Execution failed:", e, file=sys.stderr)
+    print("########################### \n",
+          "DOWNLOAD TIME: ",round((time.time()-start_time)/60.,2)," mins")
+    print("###########################")
+  
 ################################################################################# 
 # convert all files to metgrid.exe readable files and clean up downloaded files #
 #################################################################################
@@ -69,5 +91,6 @@ for fcstfile in glob.glob('GEOS.fp.fcst*'):
         print("Execution failed:", e, file=sys.stderr)
     os.remove(fcstfile)
 
-
-
+print("########################### \n",
+      "TOTAL TIME IS ",round((time.time() - start_time)/60.,2)," mins")
+print("###########################")
