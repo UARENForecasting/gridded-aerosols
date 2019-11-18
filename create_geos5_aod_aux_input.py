bb47e58c5f60a16293ef43b182bf31a797be2ac0
PatrickBunn <ptwbunn@email.arizona.edu>
2019-11-18 11:49:52 -0700

    modified script to download 00Z GEOS5 AOD for WRF 06Z and 12Z and then 12Z GEOS5 AOD for WRF 18Z and 00Z

--- create_geos5_aod_aux_input.py
+++ create_geos5_aod_aux_input.py
@@ -10,14 +10,19 @@ import time
 import numpy as np
 import netCDF4 as nc
 import pandas as pd
+# need soft link to get_aeronet.py
 from get_aeronet import mean, ang_exp
 
 #################################################################################
-# GEOS5 is initialised at 00z available to download from approx 0300-0400z      #
-# ready for inclusion in foreacsts initialised in WRF at 00z the next day.      #
+# GEOS5 is initialised at 00Z available to download from approx 0600-0700Z      #
+# ready for inclusion in foreacsts initialised in WRF at 06Z and 12Z WRF        #
+#                                                                               # 
+# 12Z GEOS5 is available to download from approx 2100-2200Z                     #
+# ready for inclusion in foreacsts initialised in WRF at 18Z and 00Z            #
 #                                                                               #
-# +84hrs of GEOS5 data is pulled, all data coming from 00z run                  # 
+# 84 + 6hrs of GEOS5 data is pulled, 84 for WRF and 6 for initialization diff.  # 
 #################################################################################
+
 # start timer for script
 start_time = time.time()
 
@@ -34,33 +39,37 @@ config = 'geos5'
 time_now = pd.Timestamp.utcnow()
 
 # testing switch
-#time_now = time_now + pd.Timedelta('1D')
+#time_now = time_now - pd.Timedelta('1D') - pd.Timedelta('12h')
+#print(time_now)
+
+# select GEOS-5 run to download data from (default is 00Z)
+geos = '00'
+# this switches to 12Z, for WRF 18Z and 00Z
+if 21 <= time_now.hour or time_now.hour < 6: 
+    geos = '12'
 
 year  = time_now.strftime('%Y')
 month = time_now.strftime('%m')
 day   = time_now.strftime('%d')
 
-#####################################################
-# download 84 + 18 = 102 hrs of 00Z GEOS-5 forecast #
-#####################################################  
-all_forecast_times = [f'{hr:02}' for hr in range(6, 22, 3)] + [f'{hr:02}' for hr in range(0, 22, 3)] * 3 + [f'{hr:02}' for hr in range(0, 10, 3)]    
-all_forecast_days = [time_now] * 6 + [time_now + pd.Timedelta('1d')] * 8 +  [time_now + pd.Timedelta('2d')] * 8 \
-                               + [time_now + pd.Timedelta('3d')] * 8 + [time_now + pd.Timedelta('4d')] * 4
-
-
-print(all_forecast_times)
-print(len(all_forecast_times))
-print(len(all_forecast_days)) 
-sys.exit()
+#################################################
+# download 84 + 6 = 90 hrs of GEOS-5 forecast   #
+#################################################  
+if geos == '00':
+    forecast_times = [f'{hr:02}' for hr in range(6, 22, 3)] + [f'{hr:02}' for hr in range(0, 22, 3)] * 3 + [f'{hr:02}' for hr in range(0, 10, 3)]    
+    forecast_days  = [time_now] * 6 + [time_now + pd.Timedelta('1d')] * 8 + [time_now + pd.Timedelta('2d')] * 8 \
+                                    + [time_now + pd.Timedelta('3d')] * 8 + [time_now + pd.Timedelta('4d')] * 4
+if geos == '12':
+    forecast_times = [f'{hr:02}' for hr in range(18, 22, 3)] + [f'{hr:02}' for hr in range(0, 22, 3)] * 3 + [f'{hr:02}' for hr in range(0, 22, 3)]    
+    forecast_days  = [time_now] * 3 + [time_now + pd.Timedelta('1d')] * 8 + [time_now + pd.Timedelta('2d')] * 8 \
+                                    + [time_now + pd.Timedelta('3d')] * 8 + [time_now + pd.Timedelta('4d')] * 7
 #################
 # download data #
 #################
-for forecast_day, forecast_time in zip(all_forecast_days, all_forecast_times):
-    if time.time() - start_time < upper_time_limit:       
-    #if time.time()-start_time > lower_time_limit  or time.time()-start_time >= upper_time_limit: 
-        file_name = 'GEOS.fp.fcst.inst1_2d_hwl_Nx.' + year + month + day + '_00+' + forecast_day.strftime('%Y%m%d') + '_' + forecast_time + '00.V01.nc4'
-        #print(file_name)
-        url = 'https://portal.nccs.nasa.gov/datashare/gmao_ops/pub/fp/forecast/Y'+year+'/M'+month+'/D'+day+'/H00/'+file_name
+for forecast_day, forecast_time in zip(forecast_days, forecast_times):
+    if time.time() - start_time < upper_time_limit:         
+        file_name = 'GEOS.fp.fcst.inst1_2d_hwl_Nx.'+year+month+day+'_'+geos+'+' + forecast_day.strftime('%Y%m%d') + '_' + forecast_time + '00.V01.nc4'
+        url = 'https://portal.nccs.nasa.gov/datashare/gmao_ops/pub/fp/forecast/Y'+year+'/M'+month+'/D'+day+'/H'+geos+'/'+file_name
         try:
             retcode = subprocess.call(" wget -nc --user=gmao_ops --password=''  "+url+" --progress=bar:force 2>&1 | tail -f -n +6 ", shell=True)
             if retcode < 0:
@@ -80,11 +89,10 @@ for forecast_day, forecast_time in zip(all_forecast_days, all_forecast_times):
 # in both instances script will populate a dummy 2D GEOS-5 file with AERONET    #
 # daily average data from UofA site                                             # 
 #################################################################################
-
 if overwrite_time_limit <= time.time()-start_time < lower_time_limit  or time.time()-start_time >= upper_time_limit: 
     config = 'aeronet'
-    for forecast_day, forecast_time in zip(all_forecast_days, all_forecast_times):      
-        out_file = 'GEOS.fp.fcst.inst1_2d_hwl_Nx.' + year + month + day + '_00_' + forecast_day.strftime('%Y%m%d') + '_' + forecast_time + '00.V01.nc4'
+    for forecast_day, forecast_time in zip(forecast_days, forecast_times):      
+        out_file = 'GEOS.fp.fcst.inst1_2d_hwl_Nx.' + year + month + day + '_'+geos+'_' + forecast_day.strftime('%Y%m%d') + '_' + forecast_time + '00.V01.nc4'
         in_file = 'dummy_geos5_data_file.nc4'
 
         subprocess.run(["cp",in_file,out_file])
@@ -103,7 +111,7 @@ if overwrite_time_limit <= time.time()-start_time < lower_time_limit  or time.ti
 ################################################################################# 
 # convert all files to metgrid.exe readable files and clean up downloaded files #
 #################################################################################
-for fcstfile in glob.glob('GEOS.fp.fcst*'):
+for fcstfile in glob.glob('GEOS.fp.fcst.inst1_2d_hwl_Nx.'+year+month+day+'_'+geos+'*'  ):
     try:
         retcode = subprocess.call("./write_aerosols_for_metgrid.Linux " + fcstfile, shell=True)
         if retcode < 0:
@@ -116,9 +124,10 @@ for fcstfile in glob.glob('GEOS.fp.fcst*'):
         os.remove(fcstfile)
 
 
+
 if config == 'geos5':
     print("########################### \n",
-          "GEOS-5 DOWNLOADED  \n",
+          "GEOS-5 DOWNLOADED FROM "+geos+"Z  \n",
           "TOTAL TIME: ",round((time.time()-start_time)/60.,2)," mins")
     print("###########################")
 if config == 'aeronet':
